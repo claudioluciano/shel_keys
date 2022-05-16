5d2ec4ef87182e807d119e3952329793b53380b6
claudioluciano <claudio_luciano@live.com>
2022-05-16 03:20:38 -0400

    chore: Fix version_bump child_process

--- scripts/version_bump.js
+++ scripts/version_bump.js
@@ -1,5 +1,5 @@
 const fs = require('fs-extra')
-const execSync = require('child_process')
+const { execSync } = require('child_process')
 
 async function resolveVersionBump () {
   const nextVersion = process.argv[2]
