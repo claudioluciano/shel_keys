3264a1379ead12cf1aae5180d250677f02f704da
claudioluciano <claudio_luciano@live.com>
2022-05-16 20:51:08 -0400

    chore: Fix version_bumper

--- scripts/version_bump.mjs
+++ scripts/version_bump.mjs
@@ -30,6 +30,7 @@ async function resolveVersionBump () {
   execSync('git add ./src-tauri/tauri.conf.json')
   execSync(`git commit -m "${nextVersion}"`)
   execSync(`git tag -a ${nextVersion} -m "${nextVersion}"`)
+  execSync('git push')
   execSync(`git push origin ${nextVersion}`)
   console.log('Publish Successfully...')
 }
