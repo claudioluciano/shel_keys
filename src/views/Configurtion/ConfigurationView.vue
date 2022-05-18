d6ef080dd384397498382a361a80de985eb81d67
claudioluciano <claudio_luciano@live.com>
2022-05-18 04:57:46 -0400

    feat: Display version

--- src/views/Configurtion/ConfigurationView.vue
+++ src/views/Configurtion/ConfigurationView.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { useShell } from '@/composables/useTauri'
+import { useSettingsManager } from '@/composables/useSettingsManager'
 const router = useRouter()
 
 function isSelectedRoute (route: string): boolean {
@@ -11,23 +13,27 @@ function openGithub () {
   useShell().openExternal('https://github.com/claudioluciano/shel_keys')
 }
 
+const version = ref(useSettingsManager().getCache('version'))
+
 </script>
 
 <template>
   <div class="flex w-screen h-screen overflow-hidden">
-    <ul class="w-56 h-full py-5 menu bg-base-200">
+    <ul class="w-56 h-full pt-5 menu bg-base-200">
       <li class="menu-title">
         <span>App Settings</span>
       </li>
-      <li :class="{ 'bordered': isSelectedRoute('configurationAppearence')}">
-        <router-link :to="{ name: 'configurationAppearence'}">
+      <li :class="{ 'bordered': isSelectedRoute('configurationAppearence') }">
+        <router-link :to="{ name: 'configurationAppearence' }">
           Appearence
         </router-link>
-      </li><li :class="{ bordered: isSelectedRoute('configurationKeybind')}">
-        <router-link :to="{ name: 'configurationKeybind'}">
+      </li>
+      <li :class="{ bordered: isSelectedRoute('configurationKeybind') }">
+        <router-link :to="{ name: 'configurationKeybind' }">
           Keybinds
         </router-link>
       </li>
+
       <li class="mt-auto hover-bordered">
         <a @click="openGithub">
           <svg
@@ -36,13 +42,20 @@ function openGithub () {
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
-          ><path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-          /></svg>
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+            />
+          </svg>
           Github
+        </a>
+      </li>
+      <li class="text-xs disabled">
+        <a class="py-1">
+          Version: {{ version }}
         </a>
       </li>
     </ul>
